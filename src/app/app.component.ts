d2d22233116098c2ac40e0598778ca41f9c419fe
yashpandit97 <yashpandit97@yashpandit-pc>
2019-09-13 19:44:48 +0530

    adding accomplished items back to the top of the todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  inputtask = null;
+  list = [];
+  task = null;
+  i: number = 0;
+  done = [];
+
+  add()
+  {
+    this.list.push(this.inputtask)
+    this.inputtask=null
+    this.i =+ 1;
+  }
+ 
+  accomplished(task)
+  {
+    this.list.splice(this.list.indexOf(task),1);
+    this.done.push(task)
+  }
+
+  remove(task)
+  {
+    this.done.splice(this.done.indexOf(task),1);
+  }
+  readd(task)
+  {
+    this.list.push(this.task)
+    this.done.splice(this.list.indexOf(task),1);
+  }
 }
